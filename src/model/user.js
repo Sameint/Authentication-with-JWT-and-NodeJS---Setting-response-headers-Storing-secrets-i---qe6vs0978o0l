3cb6ff31c7a6ea7e1a5e9e48c98889ef64012ddd
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-05-14 21:45:30 +0530

    Update user.js

--- src/model/user.js
+++ src/model/user.js
@@ -1,7 +1,40 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  //your code goes here
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
 
-module.exports = mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Generate a password hash (salt + hash)
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    // Re-assign hashed version over original, plain text password
+    this.password = passwordHash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
