a45f604e165547b3564e2aa726c88fd517e6aa39
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-05-17 21:23:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,136 +1,120 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const app = express();
-const tokenList = {}
+const tokenList = {};
 
 app.use(express.json());
-
-const auth = require("./middleware/auth");
+// const auth = require("./middleware/auth");
 
 // Your code goes here
 const User = require("./model/user");
 
-app.post("/register", async (req, res) => {
-    // register logic starts here
-     try {
-    const { first_name, last_name, email, password } = req.body;
-
-    if (!(email && password && first_name && last_name)) {
-      return res.status(400).send("Please fill up all fields");
+app.post("/api/register", async (req, res) => {
+  try {
+    let { email, password, first_name, last_name } = req.body;
+    console.log(email);
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)) {
+      return res.status(400).send("Enter correct email.");
     }
 
-    // check if user already exists
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) {
+    if (password.length < 8) {
+      return res.status(400).send("Minimum password length required is 8.");
+    }
+    let isPresent = await User.exists({ email: email });
+    if (isPresent) {
       return res.status(409).send("User Already Exist. Please Login");
     }
-
-    // hash the password
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    // create user in the database
-    const user = await User.create({
-      first_name,
-      last_name,
-      email: email.toLowerCase(),
-      password: passwordHash,
+    let hashed = await bcrypt.hash(password, 10);
+    let user = new User({
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+      password: hashed,
     });
-
-    // create a new JWT token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "6h",
-      }
-    );
-
-    // save the refresh token
-    const refreshToken = jwt.sign(
-      { user_id: user._id, email },
-      process.env.REFRESH_TOKEN_KEY
-    );
-    tokenList[refreshToken] = { email, token };
-
-    // send back the token
-    user.token = token;
-    res.status(201).json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server Error" });
+    await user.save();
+    let token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
+      expiresIn: "6h",
+    });
+    return res
+      .status(201)
+      .json({
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        password: user.password,
+        token: token,
+      });
+  } catch {
+    res.status(400).send("Please fill up all fields");
   }
 });
-
 
 // Login
-app.post("/login", async (req, res) => {
-    //login logic starts here
-    try {
-    const { email, password } = req.body;
-
-    if (!(email && password)) {
-      return res.status(400).send("All input is required");
+app.post("/api/login", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send("All input is required");
     }
-
-    // check if user exists
-    const user = await User.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // create a new JWT token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        { expiresIn: "6h" }
-      );
-
-      // save the refresh token
-      const refreshToken = jwt.sign(
-        { user_id: user._id, email },
-        process.env.REFRESH_TOKEN_KEY
-      );
-      tokenList[refreshToken] = { email, token };
-
-      // send back the token
-      res.status(200).json({ token, refreshToken });
-    } else {
-      res.status(400).send("Invalid Credentials");
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).send("Invalid Credentials");
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server Error" });
+    let isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.status(400).send("Invalid Credentials");
+    }
+    let refreshToken = jwt.sign(
+      { userId: user._id },
+      process.env.REFRESH_TOKEN_KEY,
+      {
+        expiresIn: "24h",
+      }
+    );
+    tokenList[user._id] = refreshToken;
+    let token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
+      expiresIn: "6h",
+    });
+    res.status(200).json({ token, refreshToken });
+  } catch(e) {
+    res.status(400).send("All input is required");
   }
 });
 
-app.post("/refresh", async (req, res) => {
-    // refresh token logic starts here
+app.post("/api/refresh", async (req, res) => {
+  try {
     const { email, password, refreshToken } = req.body;
-
-  // Check if refresh token is valid
-  if ((refreshToken in tokenList) && (tokenList[refreshToken] === email)) {
-    try {
-      // Verify user credentials
-      const user = await User.findOne({ email });
-      if (!user || !await bcrypt.compare(password, user.password)) {
-        return res.status(400).send("Invalid Request");
+    let user = await User.findOne({ email: email });
+    if (user) {
+      if (
+        (await bcrypt.compare(password, user.password)) &&
+        tokenList[user._id] === refreshToken
+      ) {
+        let token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
+          expiresIn: "6h",
+        });
+        res
+          .status(200)
+          .json({ email: user.email, password: user.password, token: token });
       }
-
-      // Generate new access token
-      const accessToken = jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6h' });
-
-      // Send new access token and refresh token
-      return res.status(200).json({ accessToken, refreshToken });
-    } catch (error) {
-      return res.status(500).send(error);
     }
+    res.status(404).send("Invalid Request");
+  } catch {
+    res.status(404).send("Invalid Request");
   }
+});
 
-  // Return error if refresh token is invalid
-  return res.status(404).send("Invalid Request");
-})
-
+app.post("/api/welcome", (req, res) => {
+  try {
+    let token = req.header("x-access-token");
+    jwt.verify(token, process.env.TOKEN_KEY);
+    res.send("Welcome User!");
+  } catch (error) {
+    res.status(403).send(error.message);
+  }
+});
 
 module.exports = app;
